f410f515a48cbc12497d884bf51f364adc069cd5
Willian Galvani <williangalvani@gmail.com>
2018-07-28 20:42:16 -0300

    working prototype with chartjs

--- index.js
+++ index.js
@@ -54,11 +54,88 @@ var ondrop = function (ev) {
         console.log(ev.dataTransfer.files[i]);
       }
     } 
-    
 
-    console.log("vatata");
-    console.log(app.messages);
   }
+
+var default_colors = ['#3366CC','#DC3912','#FF9900','#109618','#990099','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC']
+
+function shadeColor2(color, percent) {   
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
+var loadMessageType = function(type)
+{
+  console.log(app.messages[type]);
+  var fields = app.messages[type][0].fieldnames.slice(1);
+  console.log(fields);
+  var msgs = app.messages[type];
+  var datasets = [];
+  for (var i=0; i<fields.length; i++)
+  {
+    datasets.push({
+                          label: ''+fields[i],
+                          backgroundColor: shadeColor2(default_colors[i],0.5),
+                          borderColor: default_colors[i],
+                          fill: false,
+                          data: [],
+                        });
+  }
+
+  console.log(datasets);
+  for(msg in msgs){
+      for (field in fields){
+        datasets[field].data.push({x:msgs[msg].time_boot_ms, y: msgs[msg][fields[field]]})
+      }
+  }
+  app.plotData = {
+        datasets: datasets
+      };
+  app.plotOptions = {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Chart.js Time Point Data'
+        },
+        scales: {
+          xAxes: [{
+            type: 'time',
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Date'
+            },
+            ticks: {
+              major: {
+                fontStyle: 'bold',
+                fontColor: '#FF0000'
+              }
+            }
+          }],
+          yAxes: [{
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'value'
+            }
+          }]
+        }
+      }
+  app.shouldPlot = true;
+
+}
+
+
+
+
+Vue.component('line-chart', {
+  extends: VueChartJs.Line,
+  mounted () {
+    this.renderChart(app.plotData, app.plotOptions);
+  }
+  
+});
+
 
 
 app = new Vue({
@@ -68,7 +145,10 @@ app = new Vue({
     messages: {},
     message_types: [], 
     process: ondrop,
-    plot: function(message){console.log(message);},
+    plot: loadMessageType,
+    shouldPlot: false,
+    plotData: {},
+    plotOptions: {},
 
 
     }
